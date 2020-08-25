4b3c75fce260fb2e25d869fcfe37c6bca10402d6
pythonpete32 <pythonpete32@gmail.com>
2020-08-25 13:59:32 +0300

    edit config

--- src/index.js
+++ src/index.js
@@ -25,9 +25,9 @@ const tokenwrapperAppId =
   "0xdab7adb04b01d9a3f85331236b5ce8f5fdc5eecb1eebefb6129bc7ace10de7bd";
 const tokenwrapperBase = "0xdeFe956e2FC6c6FF6c688B9c4092457fccce13cc";
 const tokenwrapperInitSignature = "initialize(address,string,string)";
-const deposit_token = '0x9634747f6f0e74701168ce5cd6641feb11ec9c5b'; // TKN
-const token_name = "Wrapped Token";
-const symbol = "WTKN";
+const deposit_token = '0xf522b3512d936d3f809256a8b4abe2c458c58ed1'; // SAND
+const wrapped_token_name = "Wrapped Sand Token";
+const wrapped_symbol = "wSAND";
 
 
 // 3b. VOTING
@@ -35,9 +35,9 @@ const newVotingAppId =
   "0x9fa3927f639745e587912d4b0fea7ef9013bf93fb907d29faeab57417ba6e1d4";
 const newVotingBase = "0xb4fa71b3352D48AA93D34d085f87bb4aF0cE6Ab5";
 const newVotingInitSignature = "initialize(address,uint64,uint64,uint64)";
-const quorum = '250000000000000000';
-const support = '500000000000000000'
-const time = '86400';
+const quorum = '100000000000000000';    // 10%
+const support = '600000000000000000';   // 60%
+const time = '604800';                  // 1 Week
 
 
 async function createVote() {
@@ -49,8 +49,8 @@ async function createVote() {
   // 5. app initialisation payloads
   const tokenwrapperInitPayload = await encodeActCall(tokenwrapperInitSignature, [
     deposit_token,
-    token_name,
-    symbol]
+    wrapped_token_name,
+    wrapped_symbol]
   );
 
   const newVotingInitPayload = await encodeActCall(newVotingInitSignature, [
